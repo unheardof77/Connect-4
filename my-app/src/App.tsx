eb241b764fbfcbd8d092234de748fd4880193ee9
Toacin <toacinp@outlook.com>
2022-12-09 19:17:28 -0700

    board page now setup

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,19 +3,31 @@ import GameOver from "./Pages/GameOver";
 import { Route, Routes, HashRouter as Router, } from 'react-router-dom';
 import { useState } from 'react';
 import {GameProvider} from './utils/statemanagment/globalstate';
-import './utils/reset/reset.css';
+// import './utils/reset/reset.css';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
 function App() {
 
   return (
     <div className="App">
       <GameProvider>
-        <Router>
-          <Routes>
-            <Route path="/" element={<BoardPage/>}/>
-            <Route path="/gameOver/:player" element={<GameOver />}/>
-            <Route/>
-          </Routes>
-        </Router>
+      <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+          <Router>
+            <Routes>
+              <Route path="/" element={<BoardPage/>}/>
+              <Route path="/gameOver/:player" element={<GameOver />}/>
+              <Route/>
+            </Routes>
+          </Router>
+        </ThemeProvider>
       </GameProvider>
     </div>
   );
