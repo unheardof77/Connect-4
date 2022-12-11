298a6c77e79e946e7b32f5dc8448efaa5508af1c
Morgan Tolman <morgan.tolman04@gmail.com>
2022-12-11 15:23:38 -0700

    Setup apollo on front end.

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -6,6 +6,54 @@ import {GameProvider} from './utils/statemanagment/globalstate';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
+import { createClient } from 'graphql-ws';
+
+import { split, HttpLink } from '@apollo/client';
+import { getMainDefinition } from '@apollo/client/utilities';
+
+
+const httpLink = new HttpLink({
+  uri: 'graphql'
+});
+
+const wsLink = new GraphQLWsLink(createClient({
+  url: '/subscriptions',
+}));
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink,
+);
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(splitLink),
+  cache: new InMemoryCache(),
+});
+
 const darkTheme = createTheme({
   palette: {
     mode: 'dark',
@@ -16,18 +64,20 @@ function App() {
 
   return (
     <div className="App">
-      <GameProvider>
-        <ThemeProvider theme={darkTheme}>
-        <CssBaseline />
-            <Router>
-              <Routes>
-                <Route path="/" element={<BoardPage/>}/>
-                <Route path="/gameOver/:player" element={<GameOver />}/>
-                <Route/>
-              </Routes>
-            </Router>
-          </ThemeProvider>
-      </GameProvider>
+      <ApolloProvider client={client}>
+        <GameProvider>
+          <ThemeProvider theme={darkTheme}>
+          <CssBaseline />
+              <Router>
+                <Routes>
+                  <Route path="/" element={<BoardPage/>}/>
+                  <Route path="/gameOver/:player" element={<GameOver />}/>
+                  <Route/>
+                </Routes>
+              </Router>
+            </ThemeProvider>
+        </GameProvider>
+      </ApolloProvider>
     </div>
   );
 }
