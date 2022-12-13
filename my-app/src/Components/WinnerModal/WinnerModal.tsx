68a484e1a6bed8f7994bb1e21acda965829a674b
Toacin <toacinp@outlook.com>
2022-12-12 17:18:02 -0700

    added a modal for winner instead of loading to a new page

---
+++ my-app/src/Components/WinnerModal/WinnerModal.tsx
@@ -0,0 +1,53 @@
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Slide from '@mui/material/Slide';
+import { TransitionProps } from '@mui/material/transitions';
+import { forwardRef, useState, Dispatch, SetStateAction } from 'react';
+
+const Transition = forwardRef(function Transition(
+    props: TransitionProps & {
+        children: React.ReactElement<any, any>;
+    },
+    ref: React.Ref<unknown>,
+) {
+    return <Slide direction="up" ref={ref} {...props} />;
+});
+
+interface WinnerModalProps {
+    winnerModalOpen: boolean;
+    setWinnerModalOpen: Dispatch<SetStateAction<boolean>>;
+    winner: string;
+}
+
+export default function WinnerModal({ winnerModalOpen, setWinnerModalOpen, winner }: WinnerModalProps) {
+
+    const handleClose = () => {
+        setWinnerModalOpen(false);
+    };
+
+    return (
+        <Dialog
+            open={winnerModalOpen}
+            TransitionComponent={Transition}
+            keepMounted
+            onClose={handleClose}
+            aria-describedby="alert-dialog-slide-description"
+            fullWidth
+        >
+            <DialogTitle>{winner} Won!</DialogTitle>
+            <DialogContent>
+                <DialogContentText id="alert-dialog-slide-description">
+                    Congratulations
+                </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={handleClose}>Disagree</Button>
+                <Button onClick={handleClose}>Agree</Button>
+            </DialogActions>
+        </Dialog>
+    );
+}
--- my-app/src/Pages/BoardPage.tsx
+++ my-app/src/Pages/BoardPage.tsx
@@ -1,11 +1,18 @@
-import Board from "../Components/Board/Board"
-import Header from "../Components/Header/Header"
+import Board from "../Components/Board/Board";
+import Header from "../Components/Header/Header";
+import {useState} from "react";
+import WinnerModal from "../Components/WinnerModal/WinnerModal";
+
 
 export default function BoardPage() {
+    const [winner, setWinner] = useState("");
+    const [winnerModalOpen, setWinnerModalOpen] = useState(false);
+    
     return (
         <>
             <Header/>
-            <Board/>
+            <Board winner={winner} setWinner={setWinner} setWinnerModalOpen={setWinnerModalOpen}/>
+            <WinnerModal winnerModalOpen={winnerModalOpen} setWinnerModalOpen={setWinnerModalOpen} winner={winner}/>
         </>
     )
 }
