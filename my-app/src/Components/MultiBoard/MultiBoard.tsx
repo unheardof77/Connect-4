76a31a77558b24df7795de552a3645e63d6e810b
Morgan Tolman <morgan.tolman04@gmail.com>
2022-12-18 16:47:51 -0700

    Added comments for most of multiboard

--- my-app/src/Components/MultiBoard/MultiBoard.tsx
+++ my-app/src/Components/MultiBoard/MultiBoard.tsx
@@ -1,6 +1,6 @@
 import { useState, MouseEvent, Dispatch, SetStateAction, useReducer, useEffect } from "react";
 import {BsFillCircleFill} from "react-icons/bs";
-import { useGameContext, useModalContext } from "../../utils/statemanagment/globalstate";
+import { useModalContext } from "../../utils/statemanagment/globalstate";
 import '../Board/Board.css';
 import { useMutation, useSubscription } from "@apollo/client";
 import { UPDATELOBBY, SENDMESSAGE } from "../../utils/crud/Mutation";
@@ -14,26 +14,44 @@ interface Props {
 }
 
 export default function MultiBoard({winner, setWinner, playerType}:Props) {
-    const [playerTurn, setTurn] = useState(playerType === 'host');
-    const [inProgress, setInProgress] = useState(false);
-    const [state, dispatch] = useState<string[][]>([[],[],[],[],[],[],[]]);
-    const [sentMessage, setSendMessage] = useState("");
-    const { updateModalState } = useModalContext();
-    const  [ignored, forceUpdate] = useReducer(x=> x+ 1, 0);
+    const [playerTurn, setTurn] = useState(playerType === 'host');//sets initial state value for the player turn if there host they go first
+    const [inProgress, setInProgress] = useState(false); //this is the state for the animation when a piece is dropped from the top
+    const [state, dispatch] = useState<string[][]>([[],[],[],[],[],[],[]]);//local state for the game board
+    const [sentMessage, setSendMessage] = useState("");//state representing the value of the message they can send;
 
+    const { updateModalState } = useModalContext();// this is the dispatch action for the modals
+    const [ignored, forceUpdate] = useReducer(x=> x+ 1, 0);// gives us access to force rerender the game board, used when animated the pieces falling
+
+    //these two lines of code get the id and name of the game lobby and parse it
     const gameId = JSON.parse(localStorage.getItem('GameBoard') || '')._id;
     const name = JSON.parse(localStorage.getItem('GameBoard') || '').name;
 
-
+    //mutation for sending a message
     const [sendMessage] = useMutation(SENDMESSAGE);
+    //mutation for updating the lobby ie mutation thats used whenever a move is made
     const [updateLobby] = useMutation(UPDATELOBBY);
+    //subscription that listens for updated lobby data
+    const { data, loading } = useSubscription(GAMELOBBYSUB, {variables:{lobbyName:name}});
 
-    const { data, loading, error} = useSubscription(GAMELOBBYSUB, {variables:{lobbyName:name}});
+    //this use effect runs whenever data changes the if condition checks if the subscription is loading.  If it is not loading the local state gets updated to the newest gameboard and the turn is set to the opposite of what it was originally
+    useEffect(()=>{
+        if(!loading){
+            dispatch(data.gameLobbyChanged.gameboard);
+            setTurn(!playerTurn);
+        }
+    }, [data]);
 
+    //this use effect runs whenever the player turn changes this allows the board to be checked for wins after data is recieved from the subscription
+    useEffect(()=>{
+        didWin();
+    }, [playerTurn]);
+
+    //sets the value of the sentMessage state 
     const handleMessageChange = (e:React.ChangeEvent<HTMLInputElement>) =>{
         setSendMessage(e.currentTarget.value);
     };
 
+    //when the message is sent(form is submitted) it prevents a refresh and uses the sendMessage mutation to update the game lobby model in the mongodb 
     const handleMessageSubmit = async (e:React.FormEvent)=>{
         e.preventDefault();
         try{
@@ -43,26 +61,16 @@ export default function MultiBoard({winner, setWinner, playerType}:Props) {
         }
     };
 
-    useEffect(()=>{
-        if(!loading){
-            console.log(data);
-            dispatch(data.gameLobbyChanged.gameboard);
-            setTurn(!playerTurn);
-        }
-        
-    }, [data]);
 
-    useEffect(()=>{
-        didWin();
-    }, [playerTurn]);
-    
+    //creates a new state then pushes either an x or O to the column that was clicked on 
     const updateBoard = async (index:number, piece:string) =>{
         const newState = [...state];
         newState[index].push(piece);
-        dispatch(newState);
-        await updateLobby({variables:{gameboard: newState, lobbyName:name }});
+        dispatch(newState); //updates the gameboard local state to the new one
+        await updateLobby({variables:{gameboard: newState, lobbyName:name }}); //uses the updateLobby mutation to update the gameboard in mongodb
     };
 
+    // this checks if they are the host.  If they are they are represented with an x if not an O
     const whatPiece = ()=>{
         if(playerType === 'host'){
             return 'x';
@@ -72,19 +80,20 @@ export default function MultiBoard({winner, setWinner, playerType}:Props) {
     };
     const piece = whatPiece();
 
+    //this function takes in a string to test and uses regex to see if there are four x or O next to each other in the string
     const regexTest = (testString: string) => {
         const regexColX = /xxxx/;
         const regexColO = /OOOO/;
-        console.log(testString)
-        if (regexColX.test(testString)) {
+        if (regexColX.test(testString)) {// the .test returns a boolean, if its true that means x has gotten four in a row
             setWinner("Player 1");
-            updateModalState({type:'showWinnerModal'})
-        } else if (regexColO.test(testString)) {
+            updateModalState({type:'showWinnerModal'})//updates the global modal state to display the winner modal;
+        } else if (regexColO.test(testString)) {// if this test returns true then O has gotten four in a row
             setWinner("Player 2");
             updateModalState({type:'showWinnerModal'})
         };
     };
 
+    //this function looks at the gameboard state and for each array in it, it joins them into a string then runs the regexTest function on it
     const checkColWin = () => {
         state.forEach((col) => {
             const stringCol = col.join('');
@@ -92,10 +101,11 @@ export default function MultiBoard({winner, setWinner, playerType}:Props) {
         });
     };
 
+    //this function uses a for loop that represents the height of the board to check row wins for each row;
     const checkRowWin = () => {
-        for (let i = 0; i < 6; i++) {
-            const stringRow = state.map((col) => col[i] || ' ').join('');
-            regexTest(stringRow)
+        for (let i = 0; i < 6; i++) {//6 is the maximum length for each column in our game board
+            const stringRow = state.map((col) => col[i] || ' ').join('');//maps over the gameboard array and then if a piece exist it returns it otherwise it returns a space it then joins it into a string
+            regexTest(stringRow)//test each string representing rows
         };
     };
 
@@ -149,8 +159,8 @@ export default function MultiBoard({winner, setWinner, playerType}:Props) {
         };
     };
 
+    //this function acts as a controller making it quick to run every test
     function didWin() {
-        console.log('didwin did run')
         checkColWin();
         checkRowWin();
         checkDiagonalWin();
@@ -161,21 +171,22 @@ export default function MultiBoard({winner, setWinner, playerType}:Props) {
 
         setInProgress(true);
         const index = Number(e.currentTarget.getAttribute('data-index'))
+        const newState = [...state];
 
         //programatic changes to board pieces array of arrays to mock falling "animation"
-        const initialLength = state[index].length;
+        const initialLength = newState[index].length;
         for (let i:number=5; i>=initialLength; i--) {
             const ArrayToConcat: string[] = Array(i-initialLength).fill("null");
-            state[index] = piece === 'x' ? [...state[index], ...ArrayToConcat, "x"] : [...state[index], ...ArrayToConcat, "O"];
-            dispatch(state);
+            newState[index] = piece === 'x' ? [...newState[index], ...ArrayToConcat, "x"] : [...newState[index], ...ArrayToConcat, "O"];
+            dispatch(newState);
             forceUpdate();
-            await new Promise(resolve => setTimeout(resolve, 150));
-            state[index].splice(initialLength, 6);
-            dispatch(state);
+            await new Promise(resolve => setTimeout(resolve, 125));
+            newState[index].splice(initialLength, 6);
+            dispatch(newState);
             forceUpdate();
         }
 
-        if (playerTurn && state[index].length < 6) {
+        if (playerTurn && newState[index].length < 6) {
             updateBoard(index, piece);
             setInProgress(false);
         }
